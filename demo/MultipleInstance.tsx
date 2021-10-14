521ba166ccf12846009a4a83a1557cdaef32f278
superwf <superwf@gmail.com>
2021-10-14 09:46:04 +0800

    docs: update demo

--- demo/MultipleInstance.tsx
+++ demo/MultipleInstance.tsx
@@ -1,9 +1,8 @@
-import { Button, Card, Row, Spin, Table } from 'antd'
+import { Button, Card, List, Row, Typography } from 'antd'
 import { observer } from 'mobx-react-lite'
 import * as React from 'react'
 
 import { mockRequest } from './mockRequest'
-import type { User } from './type'
 
 import { mobxRequest } from '../src'
 
@@ -17,83 +16,78 @@ const updateUser = mobxRequest({
 
 const dataSource = [
   {
-    name: 'a',
+    name: 'Tom',
     key: '1',
   },
   {
-    name: 'b',
+    name: 'Jery',
     key: '2',
   },
 ]
 
-export const MultipleInstance: React.FC = observer(() => {
-  const wrongColumns = [
-    {
-      title: 'name',
-      dataIndex: 'name',
-    },
-    {
-      title: 'operation',
-      key: 'operation',
-      width: 200,
-      render() {
-        const Comp = observer(() => (
+const Wrong: React.FC = observer(() => (
+  <List>
+    <Typography.Title level={4}>Wrong Example</Typography.Title>
+    {dataSource.map(row => (
+      <List.Item
+        key={row.key}
+        actions={[
           <Button type="primary" loading={updateUser.loading} onClick={() => updateUser.request()}>
             update user
-          </Button>
-        ))
-        return <Comp />
-      },
-    },
-  ]
+          </Button>,
+        ]}
+      >
+        {row.name}
+      </List.Item>
+    ))}
+  </List>
+))
 
-  const rightColumns = [
-    {
-      title: 'name',
-      dataIndex: 'name',
-    },
-    {
-      title: 'operation',
-      key: 'operation',
-      width: 200,
-      render() {
-        const Comp = observer(() => {
-          const updateRowUser = React.useMemo(
-            () =>
-              mobxRequest({
-                value: {},
-                request: mockRequest({
-                  data: {},
-                  delay: 1000,
-                }),
-              }),
-            [],
-          )
-          return (
-            <Button type="primary" loading={updateRowUser.loading} onClick={() => updateRowUser.request()}>
-              update user
-            </Button>
-          )
-        })
-        return <Comp />
-      },
-    },
-  ]
-
+const IndependentButton: React.FC = observer(() => {
+  const updateRowUser = React.useMemo(
+    () =>
+      mobxRequest({
+        value: {},
+        request: mockRequest({
+          data: {},
+          delay: 1000,
+        }),
+      }),
+    [],
+  )
   return (
-    <Card title="Multiple instance in one page" className="text-center">
-      <Row className="mb-4" justify="center">
-        All above examples use only one data instance in one page, as singleton mode.
-        <br />
-        But when there are more same data in one component, it will be a problem.
-        <br />
-        One data instance update, all related component will update.
-      </Row>
-      <Table bordered columns={wrongColumns} dataSource={dataSource} size="small" pagination={false} />
-      <Row className="my-4" justify="center">
-        One way is use React.useMemo to create independent data instance in each component.
-      </Row>
-      <Table bordered columns={rightColumns} dataSource={dataSource} size="small" pagination={false} />
-    </Card>
+    <Button type="primary" loading={updateRowUser.loading} onClick={() => updateRowUser.request()}>
+      update user
+    </Button>
   )
 })
+
+const Right: React.FC = observer(() => (
+  <List>
+    <Typography.Title level={4}>Right Example</Typography.Title>
+    {dataSource.map(row => (
+      <List.Item key={row.key} actions={[<IndependentButton />]}>
+        {row.name}
+      </List.Item>
+    ))}
+  </List>
+))
+
+export const MultipleInstance: React.FC = observer(() => (
+  <Card title="Multiple instance in one page" className="text-center">
+    <Row className="mb-4" justify="center">
+      All above examples use only one data instance in one page, as singleton mode.
+      <br />
+      But when there are more same type data in more same type component, it will be a problem.
+      <br />
+      One data instance update, all related component will update.
+      <br />
+      It is not a good job 🤨.
+    </Row>
+    <Wrong />
+    <Row className="my-4" justify="center">
+      My way is use <b className="mx-2">React.useMemo</b> to create independent data instance in each component.
+    </Row>
+    <Right />
+  </Card>
+))
